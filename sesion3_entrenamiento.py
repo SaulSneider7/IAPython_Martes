eb0379213e87762aa4f91cd6a3ea5a2cfc9668f8
saulsneider7 <saulsneider@hotmail.com>
2023-11-21 20:03:08 -0500

    sesion3

--- sesion3_entrenamiento.py
+++ sesion3_entrenamiento.py
@@ -14,4 +14,28 @@ print('Lista de personas: ', peopleList)
 
 # -------------------------------------------------------------------------
 # Escribe tu código aquí:
+labels = []
+facesData = []
+label = 0
 
+#Creamos un buble For para poder leet todas las imagenes
+print('Leyendo las imagenes ...')
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+
+    for fileName in os.listdir(personPath):
+        print('Rostros: ', nameDir + '/' + fileName)
+
+        # Agregamos label y la imagen del rostro a su arreglo correspondiente
+        labels.append(label)
+        facesData.append(cv2.imread(personPath + '/' + fileName, 0))
+
+    # Incrementa label cuando se cambia de carpeta
+    label = labels + 1
+
+#Creamos un objeto para reconocer los rostros
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+print("Entrenando Modelo ...")
+face_recognizer.train(facesData, np.array(labels))
